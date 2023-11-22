9f4868a37544c876610f97008cfb36457c372dcc
Ravi Shankar <ivarraknash3@gmail.com>
2023-11-22 02:48:47 -0500

    Remote URL entered

--- src/Labs/a5/WorkingWithArrays.js
+++ src/Labs/a5/WorkingWithArrays.js
@@ -12,7 +12,7 @@ function WorkingWithArrays() {
         completed: false,
     });
 
-    const API = "http://localhost:4000/a5/todos";
+    const API = "https://kanbas-node-server-app-5e87.onrender.com/a5/todos";
 
     const [todos, setTodos] = useState([]);
     const postTodo = async () => {
--- src/Labs/a5/WorkingWithObjects.js
+++ src/Labs/a5/WorkingWithObjects.js
@@ -9,7 +9,7 @@ function WorkingWithObjects() {
         completed: false,
         score: 0,
     });
-    const URL = "http://localhost:4000/a5/assignment";
+    const URL = "https://kanbas-node-server-app-5e87.onrender.com/a5/assignment";
     const fetchAssignment = async () => {
         const response = await axios.get(`${URL}`);
         setAssignment(response.data);
@@ -29,26 +29,26 @@ function WorkingWithObjects() {
             <h3>Working With Objects</h3>
             <h4>Retrieving Objects</h4>
 
-            <a href="http://localhost:4000/a5/assignment"
+            <a href="https://kanbas-node-server-app-5e87.onrender.com/a5/assignment"
                 className="btn btn-primary me-2">
                 Get Assignment
             </a>
 
             <h4>Retrieving Properties</h4>
             <a
-                href="http://localhost:4000/a5/assignment/title"
+                href="https://kanbas-node-server-app-5e87.onrender.com/a5/assignment/title"
                 className="btn btn-primary me-2">
                 Get Title
             </a>
 
             <a
-                href="http://localhost:4000/a5/assignment/score"
+                href="https://kanbas-node-server-app-5e87.onrender.com/a5/assignment/score"
                 className="btn btn-primary me-2">
                 Get score
             </a>
 
             <a
-                href="http://localhost:4000/a5/assignment/completed"
+                href="https://kanbas-node-server-app-5e87.onrender.com/a5/assignment/completed"
                 className="btn btn-primary me-2">
                 Get Status
             </a>
